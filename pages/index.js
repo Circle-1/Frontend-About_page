b79b0a2b48b207a8011a1e88227fed3b47a9c472
Anurag Bagde <90402603+Yellowflash1412@users.noreply.github.com>
2021-11-01 22:56:03 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
+import styles from '../styles/Home.module.css'
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+       <h1>Stock - X</h1>
+
+    </div>
+  )
+}
